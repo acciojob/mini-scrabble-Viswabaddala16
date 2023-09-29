eb34a7e302185eb423fb7fe3e4b0ad0f7100d010
Viswabaddala16 <122138862+Viswabaddala16@users.noreply.github.com>
2023-09-29 22:39:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your code here
+const evaluatedText = document.getElementById('evaluatedText');
+  const letterCount = document.getElementById('letterCount');
+
+  evaluatedText.addEventListener('input', () => {
+    // Get the current value of the input field
+    const inputValue = evaluatedText.value;
+
+    // Count the number of characters in the input
+    const count = inputValue.length;
+
+    // Update the letter count in the h3 tag
+    letterCount.textContent = count;
+  });
